ea02b31fbab34cc2e45a25b7ef1012ed8f08a6f1
monaemhossain <monaemhs@gmail.com>
2023-08-21 14:56:04 +0300

    added enable dead button functionality, fixed some bugs with cart caculations and made some improvements to the site

--- scripts/script.js
+++ scripts/script.js
@@ -1,61 +1,91 @@
-function getValues(id){
+function getElement(id) {
     const totalAmount = document.getElementById(id);
     return totalAmount;
 }
 let total = 0;
 let totalDiscount = 0;
+let resetValue = 0;
 
-function addPrice(target){
+function addPrice(target) {
     const getPrice = target.childNodes[3].childNodes[5].innerText.split(' ')[0];
     const getPriceNumber = parseFloat(getPrice);
 
-// get product name
+    // get product name
     const listText = target.childNodes[3].childNodes[3].innerText;
-// new list item
+    // new list item
     const li = document.createElement('li');
     li.className = "item";
     li.innerText = listText;
-    const cartItem = getValues('cart-item');
+    const cartItem = getElement('cart-item');
     cartItem.appendChild(li);
-// total amount output
+    // total amount output
     total = total + getPriceNumber;
-    getValues('total-price').innerText = total.toFixed(2);
+    getElement('total-price').innerText = total.toFixed(2);
 
-// Total Price 
-    if(getValues('coupon-code').value == "SELL200"){
+    // Total Price 
+    if (getElement('coupon-code').value == "SELL200") {
         totalDiscount = total * 0.2;
-        getValues('total-discount').innerText = totalDiscount.toFixed(2);
-        getValues('grand-total').innerText = total - totalDiscount;
-    }else if(getValues('coupon-code').value !== "SELL200"){
-        getValues('total-discount').innerText = 0.00;
-        getValues('grand-total').innerText = total;
+        getElement('total-discount').innerText = totalDiscount.toFixed(2);
     }
-}
-
-
-
-function addDiscount(target){
-    if(getValues('coupon-code').value == "SELL200"){
-      totalDiscount = total * 0.2;
-      getValues('total-discount').innerText = totalDiscount.toFixed(2);
-    }else{
-        alert("invalid coupon");
-    }
-    const totalPrice = getValues('total-price').innerText;
+    const totalPrice = getElement('total-price').innerText;
     const totalPriceNumber = parseFloat(totalPrice);
     const afterDiscount = totalPriceNumber - totalDiscount;
-    getValues('grand-total').innerText = afterDiscount.toFixed(2);
+    getElement('grand-total').innerText = afterDiscount.toFixed(2);
+
+    // enable dead make purchase button
+    if (total > 0) {
+        getElement('purchase-btn').removeAttribute('disabled');
+    }
+
 }
 
-function resetCart(){
-    
-    getValues('coupon-code').value = '';
-    getValues('total-price').innerText = 0.00;
-    getValues('total-discount').innerText = 0.00;
-    getValues('grand-total').innerText = 0.00;
+// calculate discount
+function addDiscount(target) {
+    if (getElement('coupon-code').value == "SELL200") {
+        totalDiscount = total * 0.2;
+        getElement('total-discount').innerText = totalDiscount.toFixed(2);
+        
+        target.parentNode.className = "join w-full text-lime-600";
+        target.parentNode.childNodes[1].className = "input input-bordered join-item focus:outline-none border-lime-600 w-full"
+    } else {
+        target.parentNode.className = "join w-full text-rose-500 border-red-500 outline-2 outline-rose-500";
+        target.parentNode.childNodes[1].className = "input join-item focus:outline-2 outline-rose-500 w-full border-rose-600"
+    }
+    const totalPrice = getElement('total-price').innerText;
+    const totalPriceNumber = parseFloat(totalPrice);
+    const afterDiscount = totalPriceNumber - totalDiscount;
+    getElement('grand-total').innerText = afterDiscount.toFixed(2);
+}
+
+
+// Reset Cart
+function resetCart() {
+
+    getElement('coupon-code').value = '';
+    getElement('total-price').innerText = resetValue.toFixed(2);
+    getElement('total-discount').innerText = resetValue.toFixed(2);
+    getElement('grand-total').innerText = resetValue.toFixed(2);
+
+    total = 0;
+    totalDiscount = 0;
+    resetValue = 0;
+
     const removableList = document.querySelectorAll('#cart-item li');
 
-    for(let i = 0; i<removableList.length; i++){
+    for (let i = 0; i < removableList.length; i++) {
         removableList[i].remove();
     }
 }
+
+// enable dead coupon button
+function enableBtn() {
+    const couponInput = getElement('coupon-code')
+    const couponInputValue = couponInput.value;
+    if (couponInputValue === 'SELL200') {
+        getElement('apply-discount').removeAttribute('disabled');
+    } else {
+        // getElement('apply-discount').setAttribute('disabled', true)
+    }
+}
+
+
